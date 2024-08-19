dbf9c7ed08af2865f6f4a5798cea49f8e8ba56d0
woo-be <150896971+woo-be@users.noreply.github.com>
2024-08-19 17:31:35 +0900

    [Bronze II] Title: 수 정렬하기, Time: 136 ms, Memory: 15800 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -12,14 +12,15 @@ public class Main {
 
     int n = Integer.parseInt(br.readLine());
     int[] arr = new int[n];
-    int length = 1;
     arr[0] = Integer.parseInt(br.readLine());
+    int length = 1; // 배열의 길이
+
     for (int i = 0; i < n - 1; i++) {
       int input = Integer.parseInt(br.readLine());
-      boolean b = false;
+      boolean b = false; // 입력받은 수가 배열의 있는 수보다 클 경우 true, 아니면 false
       for (int j = 0; j < length; j++) {
         if (input < arr[j]) {
-
+          // 배열의 가장 뒤부터 1씩 뒤로 밀기, 입력받은 수보다 큰 수들만 밀기.
           for (int k = 0; k < length - j; k++) {
             arr[length - k] = arr[length - k - 1];
           }
@@ -30,7 +31,7 @@ public class Main {
           break;
         }
       }
-      if (!b) {
+      if (!b) { // false면 배열의 끝에 입력값을 넣음.
         arr[length] = input;
         length++;
       }
