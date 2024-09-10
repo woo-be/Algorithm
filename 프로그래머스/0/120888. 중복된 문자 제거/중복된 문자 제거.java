234bc324899b75c7e4d9dc6ad38d5f3532fc5392
woo-be <150896971+woo-be@users.noreply.github.com>
2024-09-10 23:11:03 +0900

    [level 0] Title: 중복된 문자 제거, Time: 1.81 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -1,43 +1,12 @@
 class Solution {
     public String solution(String my_string) {
-        int[] uppercase = new int[27];
-        int[] lowercase = new int[27];
-        int space = 0;
-        int length = my_string.length();
-        for (int i = 0; i < length; i++) {
-            char c = my_string.charAt(i);
-            if (c >= 'a' && c <= 'z') {
-                
-                if (lowercase[c - 'a'] == 0) {
-                    lowercase[c - 'a']++;
-                } else {
-                    my_string = my_string.substring(0, i) + my_string.substring(i + 1);
-                    length--;
-                    i--;
-                }
-                
-            } else if (c >= 'A' && c <= 'Z') {
-                
-                if (uppercase[c - 'A'] == 0) {
-                    uppercase[c - 'A']++;
-                } else {
-                    my_string = my_string.substring(0, i) + my_string.substring(i + 1);
-                    length--;
-                    i--;
-                }
-                
-            } else if (c == ' ') {
-                
-                if (space == 0) {
-                    space++;
-                } else {
-                    my_string = my_string.substring(0, i) + my_string.substring(i + 1);
-                    length--;
-                    i--;
-                }
+        String answer = "";
+        for (int i = 0; i < my_string.length(); i++) {
+            String c = String.valueOf(my_string.charAt(i));
+            if (!answer.contains(c)) {
+                answer += c;
             }
-            
         }
-        return my_string;
+        return answer;
     }
 }
